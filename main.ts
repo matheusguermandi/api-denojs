4903f736bc2de0666962ea01112cd98210d329f8
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-31 19:33:46 -0300

    Create a POST route with data

--- main.ts
+++ main.ts
@@ -1,19 +1,30 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts"
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
 const router = new Router();
 
-router.get('/user', (ctx) =>{
-    ctx.response.body = "I am a user";  
-})
+router.get("/user", (context) => {
+  const user = {
+    name: "Matheus Guermandi",
+    email: "matheus_guermandi@hotmail.com",
+  };
+  context.response.body = user;
+});
+
+router.post("/user", async (context) => {
+  const { value } = await context.request.body();
+
+  context.response.status = 201;
+  context.response.body = value;
+});
 
 app.use(router.routes());
 
-app.use((ctx) => {
-    ctx.response.body = "Hello Deno From OAK";
+app.use((context) => {
+  context.response.body = "Hello Deno From OAK";
 });
 
 console.log("server started at http://localhost:4000");
 
-await app.listen({ port: 4000});
+await app.listen({ port: 4000 });
