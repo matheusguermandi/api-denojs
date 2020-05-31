b0f8684e105c8d940827d3da9d247c2433f39c28
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-31 18:33:30 -0300

    Getting Started with Deno

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-console.log('Hello Deno from file');
+import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
+
+const server = serve({port: 8000});
+
+console.log("http://localhost:8000/");
+
+for await (const req of server){
+    req.respond({ body: "Hello Deno\n"}); 
+}
