43957c5ab22f2bea6d1432b2e25997f693a097ad
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-31 20:35:21 -0300

    Using Environment Variable

--- main.ts
+++ main.ts
@@ -1,15 +1,20 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 import router from "./routes.ts";
 import notFound from "./404.ts";
 
-
 const app = new Application();
+
+const env = config();
+
+const HOST = env.APP_HOST || "http://localhost";
+const PORT = +env.APP_PORT || 4000;
 
 app.use(router.routes());
 
 app.use(notFound);
 
-console.log("server started at http://localhost:4000");
+console.log(`server started at ${HOST}:${PORT}`);
 
-await app.listen({ port: 4000 });
+await app.listen({ port: PORT });
