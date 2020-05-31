1791bdbce5d7f81d2bcce5bc3217910e1c26667a
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-31 20:06:04 -0300

    Handle not found

---
+++ 404.ts
@@ -0,0 +1,4 @@
+export default ({ response }: { response: any }) => {
+  response.status = 404;
+  response.body = { error: "Not Found" };
+};
--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 import router from "./routes.ts";
+import notFound from "./404.ts";
+
 
 const app = new Application();
 
 app.use(router.routes());
 
-app.use((context) => {
-  context.response.body = "Hello Deno From OAK";
-});
+app.use(notFound);
 
 console.log("server started at http://localhost:4000");
 
