fe0c67da075768e7d99b418c7b360f4d187750d8
matheusguermandi <matheus_guermandi@hotmail.com>
2020-06-02 19:40:17 -0300

    Multiple Errors at once

--- validation.ts
+++ validation.ts
@@ -1,5 +1,8 @@
 export default {
   async validate(context: any) {
+    let errors = [];
+    let status;
+
     const { value } = await context.request.body();
 
     if (!value) {
@@ -12,13 +15,14 @@ export default {
 
     for (let index = 0; index < fields.length; index++) {
       if (!value[fields[index]]) {
-        context.response.status = 422; // unprocessable entity
-        context.response.body = {
-          error: { message: `${fields[index]} fiel is required` },
-        };
-
-        return false;
+        status = 422; // unprocessable entity
+        errors.push({ [fields[index]]: `${fields[index]} fiel is required` });
       }
+    }
+
+    if(status){
+        context.response.body = {errors};
+        return false;
     }
 
     return value;
