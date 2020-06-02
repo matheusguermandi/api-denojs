e14f44dad05d2b9eda0056ec48e3779d048f5b62
matheusguermandi <matheus_guermandi@hotmail.com>
2020-06-01 23:35:16 -0300

    Validate Request Data

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -17,7 +17,35 @@ export default {
   },
 
   async store(context: any) {
+    if (!context.request.hasBody) {
+      context.response.status = 400; //bad request
+      context.response.body = { error: "Please provide the required data" };
+
+      return;
+    }
+
     const { value } = await context.request.body();
+
+    if (!value.email) {
+      context.response.status = 422; // unprocessable entity
+      context.response.body = { error: { message: "Email fiel is required" } };
+      
+      return;
+    }
+
+    if (!value.name) {
+      context.response.status = 422; // unprocessable entity
+      context.response.body = { error: { message: "Name fiel is required" } };
+      
+      return;
+    }
+
+    if (!value.password) {
+      context.response.status = 422; // unprocessable entity
+      context.response.body = { error: { message: "Password fiel is required" } };
+      
+      return;
+    }
 
     const insertIn = await user.insertOne(value);
 
