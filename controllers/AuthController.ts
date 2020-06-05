afc963f1b58db484630b2d3b6a74630b2cd503af
matheusguermandi <matheus_guermandi@hotmail.com>
2020-06-04 22:25:34 -0300

    Update verify Password

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -8,15 +8,33 @@ export default {
   async login(context: any) {
     const value = await validation.validateLogin(context);
 
-    if (value) {
-      const user = await userCollection.findOne({ email: value.email });
-      let passwordMatched = false;
-
-      if(user){
-        passwordMatched = await hash.verify(user.password,value.password);
-      }
-      
-      context.response.body = passwordMatched;
+    if (!value) {
+      context.response.status = 422;
+      context.response.body = { error: "Please provider required data" };
+      return;
     }
+
+    const user = await userCollection.findOne({ email: value.email });
+
+    if (!user) {
+      context.response.status = 422;
+      context.response.body = {
+        error: "Credentials doesn't match out record",
+      };
+      return;
+    }
+
+    const passwordMatched = await hash.verify(user.password, value.password);
+
+    if (!passwordMatched) {
+      context.response.status = 422;
+      context.response.body = {
+        error: "Password is incorrect",
+      };
+      return;
+    }
+
+    context.response.body = user;
+
   },
 };
